7458e070ee2f1d6f87b04b5cc6d4e8acbf9afde2
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2023-03-03 11:50:56 +0000

    jes

--- scripts/Serbulo.js
+++ scripts/Serbulo.js
@@ -2,7 +2,8 @@ const hellzero = extend(SectorPreset, "hell-zero", Serbulo, 69, {
     localizedName: "Hell zero",
     description: "Test des, there is no way im gonna waste my time writing there if it doesn't work",
     details: "Test des, there is no way im gonna waste my time writing there if it doesn't work",
-    difficulty: 2
+    difficulty: 2,
+    alwaysUnlocked: true
 });
 module.exports = {
     hellzero: hellzero
